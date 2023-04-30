2ce79024fe1a0a34218ea5dae295a25348f8033d
jaaaco <jakub@wietrzyk.com>
2023-04-30 17:13:37 +0200

    state examples

--- src/App.js
+++ src/App.js
@@ -1,25 +1,70 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from 'react';
+
+const ELEMENT_COUNT = 100;
+
+function MyCounter ({ numb, count, onClick }) {
+  
+  console.info({ numb, count });
+  return (
+    <p>
+      The button no. {numb} was clicked {count} times.
+      <button onClick={onClick}> Click me! </button>
+    </p>
+  )
+}
 
 function App() {
+  const [counts, setCounts] = useState(Array(ELEMENT_COUNT).fill(0))
+
+  const incCouner = (whichCounter) => () => {
+    const countsCopy = [...counts]
+    countsCopy[whichCounter] += 1
+    setCounts(countsCopy)
+  }
+
+  // Array(ELEMENT_COUNT).fill(0)
+  // - to robi [0, 0, 0, 0, ...]
+
+
+  // Array(ELEMENT_COUNT).fill().map((x,i)=>i) 
+  // - to robi [0, 1, 2, ...]
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+        {Array(ELEMENT_COUNT).fill().map((x,i)=>i).map((i, index) => <MyCounter key={index} count={counts[i]} numb={index +1} onClick={incCouner(i)} />)}
+        <button onClick={() => {
+          setCounts(Array(ELEMENT_COUNT).fill(0))
+        }}>Reset ALL</button>
+    </>
   );
 }
 
 export default App;
+
+
+// TO JEST TO SAMO:
+
+function zrobCosTam(par1, par2) {
+  alert(par1 + par2)
+}
+
+const zrobCosTam2 = function(par1, par2) {
+  alert(par1 + par2)
+}
+
+const zrobCosTam3 = (par1, par2) => {
+  alert(par1 + par2)
+}
+
+
+// TO JEST TO SAMO:
+
+function zrobCosTamInnego(parX) {
+  return function zrobJeszczeCostTam(parX, par1, par2) {
+
+  }
+}
+
+const zrobCosTamInnego2 = (parX) => (par1, par2) => {
+  alert(par1 + par2)
+}
